48576edf2236386ca37a5c9f7d7d01a0bac02dd6
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-11-24 11:55:38 +0200

    add components

---
+++ src/components/header/burgerMenuBtn/BurgerMenuBtn.jsx
@@ -0,0 +1,13 @@
+import BurgerMenu from 'components/header/burgerMenu/BurgerMenu';
+import React from 'react';
+
+const BurgerMenuBtn = () => {
+  return (
+    <div>
+      <button>brgr</button>
+      <BurgerMenu />
+    </div>
+  );
+};
+
+export default BurgerMenuBtn;
