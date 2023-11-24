f9c1095ca4b0e19602c1fc63eeece00a9ff3573b
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-11-24 10:03:02 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './Layout';
-import Test from 'pages/test/test';
+import Header from './header/Header';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Test />} />
+        <Route
+          index
+          element={
+            <div>
+              <Header />
+            </div>
+          }
+        />
       </Route>
     </Routes>
   );
