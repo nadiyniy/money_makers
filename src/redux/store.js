617c41fa9a3445440a6cb4c8067024af95f63207
Misetr <135700846+Misetr@users.noreply.github.com>
2023-11-24 16:51:09 +0200

    add import

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { authReducer } from './auth/slice';
+import { userReducer } from './user/slice';
 const authPersistConfig = {
   key: 'auth',
   storage,
