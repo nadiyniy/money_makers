57b6a297d059a036fbb8db226c8ced3aded1e5e3
MariiaHula <mariyagulaya@gmail.com>
2023-11-25 14:28:17 +0200

    fix operations and at trans form added required

--- src/pages/Home/MainTransactionsPage.jsx
+++ src/pages/Home/MainTransactionsPage.jsx
@@ -3,18 +3,25 @@ import TransactionsTotalAmount from '../../shared/TransactionsTotalAmount/Transa
 import TransactionsChart from './TransactionsChart';
 import TransactionForm from '../../shared/TransactionForm/TransactionForm';
 import { useParams } from 'react-router-dom';
+import { StyledCommonWrapper } from 'styles/Common.styled';
 
 const MainTransactionsPage = () => {
   const { transactionsType } = useParams();
 
   return (
-    <div>
-      <h1>{transactionsType === 'incomes' ? 'Income' : 'Expense'} Log</h1>
-      <p>Capture and organize every penny spent with ease! A clear view of your financial habits at your fingertips.</p>
-      <TransactionsTotalAmount />
-      <TransactionsChart transactionsType={transactionsType} />
-      <TransactionForm transactionsType={transactionsType} />
-    </div>
+    <section>
+      <StyledCommonWrapper>
+        <div>
+          <h1>{transactionsType === 'incomes' ? 'Income' : 'Expense'} Log</h1>
+          <p>
+            Capture and organize every penny spent with ease! A clear view of your financial habits at your fingertips.
+          </p>
+          <TransactionsTotalAmount />
+          <TransactionsChart transactionsType={transactionsType} />
+          <TransactionForm transactionsType={transactionsType} />
+        </div>
+      </StyledCommonWrapper>
+    </section>
   );
 };
 
