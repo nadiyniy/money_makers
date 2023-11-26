c52921a70e8e8d5041467550bb53a0bd882fd616
OlenaPobehailo <olena.pobehailo@gmail.com>
2023-11-26 13:55:12 +0200

    logic of rendering BgImageWrapper

---
+++ src/shared/BgImageWrapper/BgImageWrapper.jsx
@@ -0,0 +1,15 @@
+
+const BgImageWrapper = () => {
+  return (
+    <div>
+      <h1
+        //todo  DELETE styles
+        style={{  color: 'blue' }}
+      >
+       BgImageWrapper
+      </h1>
+    </div>
+  );
+};
+
+export default BgImageWrapper;
