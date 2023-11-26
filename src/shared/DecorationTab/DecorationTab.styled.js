cc8bdfa1e5dc2c58cff165dfe279ad3a05274791
OlenaPobehailo <olena.pobehailo@gmail.com>
2023-11-26 20:43:57 +0200

    styles for tablet

--- src/shared/DecorationTab/DecorationTab.styled.js
+++ src/shared/DecorationTab/DecorationTab.styled.js
@@ -6,8 +6,7 @@ export const StyledDecorationTab = styled.div`
   bottom: 60px;
 
   display: flex;
-  /* justify-content: space-between; */
-  /* align-items: ; */
+  align-items: center;
   gap: 12px;
 
   width: 248px;
@@ -16,6 +15,17 @@ export const StyledDecorationTab = styled.div`
   border-radius: 20px;
 
   background: var(--white-color);
+
+  @media screen and (min-width: 768px) {
+    left: -16px;
+    bottom: 46px;
+
+    gap: 22px;
+
+    width: 303px;
+    height: 121px;
+    padding: 32px 23px;
+  }
 `;
 
 export const IconWrapper = styled.div`
@@ -27,6 +37,11 @@ export const IconWrapper = styled.div`
   height: 36px;
   border-radius: 10px;
   background: var(--primary-color);
+
+  @media screen and (min-width: 768px) {
+    width: 43px;
+    height: 43px;
+  }
 `;
 
 export const Flex = styled.div`
@@ -34,12 +49,22 @@ export const Flex = styled.div`
   justify-content: center;
   align-items: center;
   gap: 20px;
+
+  @media screen and (min-width: 768px) {
+    gap: 22px;
+  }
 `;
 
 export const Text = styled.p`
   margin-bottom: 4px;
   color: rgba(17, 16, 28, 0.5);
   font-size: 12px;
+  margin-bottom: 4px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 16px;
+    margin-bottom: 7px;
+  }
 `;
 
 export const Amount = styled.p`
@@ -47,6 +72,11 @@ export const Amount = styled.p`
 
   font-size: 18px;
   font-weight: 700;
+  line-height: 1.2;
+
+  @media screen and (min-width: 768px) {
+    font-size: 24px;
+  }
 `;
 
 export const Percent = styled.p`
