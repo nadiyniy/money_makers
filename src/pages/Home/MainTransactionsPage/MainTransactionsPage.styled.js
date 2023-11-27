3706cce14500484e9ffce79d7cd7281947c7b89f
MariiaHula <mariyagulaya@gmail.com>
2023-11-27 11:26:16 +0200

    Update MainTransactionsPage.styled.js

--- src/pages/Home/MainTransactionsPage/MainTransactionsPage.styled.js
+++ src/pages/Home/MainTransactionsPage/MainTransactionsPage.styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+<<<<<<< Updated upstream
 export const MainPageSection = styled.section`
   @media screen and (min-width: 1280px) {
     max-height: 800px;
@@ -14,12 +15,25 @@ export const MainPageWrapper = styled.div`
   @media screen and (min-width: 1280px) {
     flex-direction: row;
     flex-wrap: wrap;
+=======
+export const MainPageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 335px;
+  @media screen and (min-width: 768px) {
+  }
+  @media screen and (min-width: 1280px) {
+>>>>>>> Stashed changes
   }
 `;
 export const MainPageTitle = styled.h1`
   color: var(--white-color);
+<<<<<<< Updated upstream
   margin-top: 66px;
   margin-bottom: 20px;
+=======
+  font-family: Suisse Intl;
+>>>>>>> Stashed changes
   font-size: 32px;
   font-style: normal;
   font-weight: 400;
@@ -27,6 +41,7 @@ export const MainPageTitle = styled.h1`
   letter-spacing: -0.64px;
 
   @media screen and (min-width: 768px) {
+<<<<<<< Updated upstream
     max-width: 500px;
     font-size: 38px;
     letter-spacing: -0.76px;
@@ -35,11 +50,19 @@ export const MainPageTitle = styled.h1`
   }
   @media screen and (min-width: 1280px) {
     max-width: 630px;
+=======
+    font-size: 38px;
+    letter-spacing: -0.76px;
+>>>>>>> Stashed changes
   }
 `;
 export const MainPageText = styled.p`
   color: var(--gray-text-40);
+<<<<<<< Updated upstream
   margin-bottom: 40px;
+=======
+  font-family: Suisse Intl;
+>>>>>>> Stashed changes
   font-size: 14px;
   font-style: normal;
   font-weight: 400;
@@ -47,6 +70,7 @@ export const MainPageText = styled.p`
   letter-spacing: -0.28px;
 
   @media screen and (min-width: 768px) {
+<<<<<<< Updated upstream
     max-width: 500px;
     font-size: 16px;
     letter-spacing: -0.32px;
@@ -54,4 +78,9 @@ export const MainPageText = styled.p`
   @media screen and (min-width: 1280px) {
     max-width: 630px;
   }
+=======
+    font-size: 16px;
+    letter-spacing: -0.32px;
+  }
+>>>>>>> Stashed changes
 `;
