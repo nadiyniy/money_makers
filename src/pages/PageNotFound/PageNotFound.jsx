ac4e74572e6f1d1bc073394c0bc9f0e1ccd12b8d
Misetr <135700846+Misetr@users.noreply.github.com>
2023-11-27 08:51:58 +0200

    pageNotFound

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,30 +1,52 @@
-import React from 'react';
+//import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { StyledBox, StyledTitle, StyledLink } from './PageNotFound.styled';
+import { Link } from 'react-router-dom';
 
-// import { StyledLinkHome, StyledWrapper, StyledHomeLink } from './PageNotFound.styled';
+const TypingEffect = ({ text, speed }) => {
+  const [displayedText, setDisplayedText] = useState('');
 
-// const PageNotFound = () => {
-//   return (
-//     <StyledWrapper>
-//       <h1>Alarm! 404</h1>
-//       <StyledLinkHome>
-//         You can go to <StyledHomeLink to="/">Home</StyledHomeLink>
-//       </StyledLinkHome>
-//     </StyledWrapper>
-//   );
-// };
+  useEffect(() => {
+    let index = 0;
+    const intervalId = setInterval(() => {
+      setDisplayedText((prevText) => prevText + (text[index] || ''));
+      index++;
+      if (index > text.length) clearInterval(intervalId);
+    }, speed);
 
-// export default PageNotFound;
+    return () => clearInterval(intervalId);
+  }, [text, speed]);
 
-import img from '../../images/404-page.png';
-import { StyledNotFoundImg, StyledWrapper } from './PageNotFound.styled';
+  return <span>{displayedText}</span>;
+};
 
 const PageNotFound = () => {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  const handleMouseMove = e => {
+    const offsetX = (e.nativeEvent.offsetX / e.target.offsetWidth - 0.5) * 20;
+    const offsetY = (e.nativeEvent.offsetY / e.target.offsetHeight - 0.5) * 20;
+    setX(offsetX);
+    setY(offsetY);
+  };
+
   return (
-    <StyledWrapper>
-      <h1>Alarm! 404</h1>
-      <StyledNotFoundImg src={img} alt="404" />
-    </StyledWrapper>
+    <StyledBox
+      onMouseMove={handleMouseMove}
+      style={{
+        textShadow: `${x}px ${y}px 10px rgba(255, 255, 255, 0.5)`,
+      }}
+    >
+      <StyledTitle>
+        <TypingEffect text=" 4404 Page Not Found " speed={150} />
+      </StyledTitle>
+      <StyledLink to={'/'}>
+        home page
+      </StyledLink> 
+    </StyledBox>
   );
 };
 
 export default PageNotFound;
+
--- src/pages/PageNotFound/PageNotFound.styled.js
+++ src/pages/PageNotFound/PageNotFound.styled.js
@@ -1,26 +1,36 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-// import { Link } from 'react-router-dom';
 
-export const StyledWrapper = styled.div`
-  // display: grid;
-  min-height: 100vh;
-  place-content: center;
-  margin-left: auto;
-  margin-right: auto;
-  h1 {
-    font-weight: 500;
-    font-size: 48px;
-    text-align: center;
-    margin-left: auto;
-    margin-right: auto;
-    color: var(--error-color);
-  }
+export const StyledTitle = styled.h1`
+  font-size: 80px; /* Increased size for a more prominent display */
+  color: #ffffff;
+  text-transform: uppercase; /* Makes all letters uppercase */
+  font-family: 'Press Start 2P', cursive; /* This font has a pixelated style */
+  margin-bottom: 20px; /* Adjust as needed */
+  
 `;
 
-export const StyledNotFoundImg = styled.img`
-  object-fit: cover;
-  width: 100%;
-  height: 100%;
+export const StyledBox = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-color: #0C0D0D;
+  /* Removed text shadow for a cleaner look */
+`;
+
+export const StyledLink = styled(Link) `
+  text-decoration: underline;
+  color: #0EF387; /* Bright green color for the link */
+  font-family: 'Press Start 2P', cursive; /* Consistent font for the link */
+  font-size: 24px; /* Adjust as needed */
+  cursor: pointer; /* Changes the cursor to indicate clickable */
+   display: block;
+   margin-right: auto;
+   margin-left: auto;
 `;
 
 
