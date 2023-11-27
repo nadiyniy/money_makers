66a12c9e8922125ba4aba238cabb6917fe890d5b
Andrii Bilyk <bilykaim@gmail.com>
2023-11-27 12:22:32 +0200

    updates svg

--- src/components/svgs/index.js
+++ src/components/svgs/index.js
@@ -1,27 +1,31 @@
 export { ReactComponent as UserAvatarIcon } from './icons/user-avatar.svg';
+export { ReactComponent as UserSettings } from './icons/user-settings.svg';
+export { ReactComponent as UserSettingsColor } from './icons/user-settings-color.svg';
+
 export { ReactComponent as CloseIcon } from './icons/close.svg';
-export { ReactComponent as ArrowRightUpIcon } from './icons/Arrow 15.svg';
+export { ReactComponent as CloseModalIcon } from './icons/close-modal.svg';
+export { ReactComponent as CloseErrorLogin } from './icons/close-error-login.svg';
 
+export { ReactComponent as ArrowUpRight } from './icons/arrow-up-right.svg';
+export { ReactComponent as ArrowDownLeft } from './icons/arrow-down-left.svg';
 export { ReactComponent as ArrowUpCurrency } from './icons/arrow-up_currency.svg';
-export { ReactComponent as Calendar } from './icons/calendar.svg';
-export { ReactComponent as Search } from './icons/search.svg';
+export { ReactComponent as ArrowDownCarrency } from './icons/arrow-down_carrency.svg';
+export { ReactComponent as ArrowUpColor } from './icons/arrow-up.svg';
+export { ReactComponent as ArrowDownColor } from './icons/arrow-down.svg';
 
-// export { ReactComponent as  } from './icons/';
-// export { ReactComponent as  } from './icons/';
-// export { ReactComponent as  } from './icons/';
-// export { ReactComponent as  } from './icons/';
-// export { ReactComponent as  } from './icons/';
-// export { ReactComponent as  } from './icons/';
-// export { ReactComponent as  } from './icons/';
-// export { ReactComponent as  } from './icons/';
-// export { ReactComponent as  } from './icons/';
-// export { ReactComponent as  } from './icons/';
-// export { ReactComponent as  } from './icons/';
-// export { ReactComponent as  } from './icons/';
-// export { ReactComponent as  } from './icons/';
-// export { ReactComponent as  } from './icons/';
-// export { ReactComponent as  } from './icons/';
-// export { ReactComponent as  } from './icons/';
-// export { ReactComponent as  } from './icons/';
-// export { ReactComponent as  } from './icons/';
-// export { ReactComponent as  } from './icons/';
+export { ReactComponent as CalendarIcon } from './icons/calendar.svg';
+export { ReactComponent as CalendarColorIcon } from './icons/calendar-color.svg';
+export { ReactComponent as ClockIcon } from './icons/clock.svg';
+export { ReactComponent as SearchIcon } from './icons/search.svg';
+export { ReactComponent as PencilIcon } from './icons/pencil.svg';
+export { ReactComponent as LogOutIcon } from './icons/log-out.svg';
+export { ReactComponent as ProjectIcon } from './icons/project-icon.svg';
+export { ReactComponent as MenuIcon } from './icons/menu.svg';
+export { ReactComponent as DeleteIcon } from './icons/delete.svg';
+
+export { ReactComponent as EyeIcon } from './icons/eye.svg';
+export { ReactComponent as EyeOffIcon } from './icons/eye-off.svg';
+
+export { ReactComponent as CheckIcon } from './icons/check.svg';
+export { ReactComponent as CheckCorrectLogin } from './icons/check-correct-login.svg';
+
