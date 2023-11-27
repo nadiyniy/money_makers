394cd5b69600e40e989621abab84c11dc5a03dac
pinkyjohnny <136741239+pinkyjohnny@users.noreply.github.com>
2023-11-27 19:40:06 +0200

    full styled page

--- src/pages/TransactionsHistory/TransactionsHistory.jsx
+++ src/pages/TransactionsHistory/TransactionsHistory.jsx
@@ -7,12 +7,18 @@ import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router';
 import { fetchTransactionsThunk } from 'redux/transactions/operations';
 
-// import { selectTransactions, selectTransactionsLoading } from 'redux/transactions/selectors';
-
 import TransactionsTotalAmount from 'shared/TransactionsTotalAmount/TransactionsTotalAmount';
 import {
+  ActionButtonWrapper,
   Button,
+  Container,
+  DeleteButton,
+  EditButton,
+  IconWrapper,
   Input,
+  LeftSideContainer,
+  RightSideContainer,
+  SideContainer,
   StyledBackground,
   StyledForm,
   StyledFormWrapper,
@@ -24,46 +30,48 @@ import {
   Title,
 } from './TransactionsHistoryStyled';
 import { StyledCommonWrapper } from 'styles/Common.styled';
-import { Calendar, Search } from 'components/svgs';
+import { Calendar, Search, TrPencil, TrTrash } from 'components/svgs';
 
 const TransactionsHistoryPage = () => {
-  // const transactions = useSelector(selectTransactions);
-  //   const isLoading = useSelector(selectTransactionsLoading);
   const dispatch = useDispatch();
   const { transactionsType } = useParams();
   useEffect(() => {
     dispatch(fetchTransactionsThunk(transactionsType));
   }, [dispatch, transactionsType]);
 
-  //   const getAllExpenses = type => {
-  //     dispatch(fetchTransactionsThunk(type));
-  //   };
-
   const [startDate, setStartDate] = useState(new Date());
   return (
     <StyledCommonWrapper>
-      <Title> All {transactionsType === 'incomes' ? 'Income' : 'Expense'}</Title>
-      <Text>View and manage every transaction seamlessly! Your entire financial landscape, all in one place.</Text>
-      <TransactionsTotalAmount />
-      <StyledBackground>
-        <StyledForm>
-          <Input type="text" placeholder="Search for anything..." />
-          <Button>
-            <Search />
-          </Button>
-        </StyledForm>
-        <StyledFormWrapper>
-          <DatePicker
-            wrapperClassName="datePicker"
-            dateFormat="dd/MM/yyyy"
-            selected={startDate}
-            onChange={date => setStartDate(date)}
-          />
-          <Button>
-            <Calendar />
-          </Button>
-        </StyledFormWrapper>
+      <SideContainer>
+        <LeftSideContainer>
+          <Title> All {transactionsType === 'incomes' ? 'Income' : 'Expense'}</Title>
+          <Text>View and manage every transaction seamlessly! Your entire financial landscape, all in one place.</Text>
+        </LeftSideContainer>
+        <RightSideContainer>
+          <TransactionsTotalAmount />
+        </RightSideContainer>
+      </SideContainer>
 
+      <StyledBackground>
+        <Container>
+          <StyledForm>
+            <Input type="text" placeholder="Search for anything..." />
+            <Button>
+              <Search />
+            </Button>
+          </StyledForm>
+          <StyledFormWrapper>
+            <DatePicker
+              wrapperClassName="datePicker"
+              dateFormat="dd/MM/yyyy"
+              selected={startDate}
+              onChange={date => setStartDate(date)}
+            />
+            <IconWrapper>
+              <Calendar />
+            </IconWrapper>
+          </StyledFormWrapper>
+        </Container>
         <StyledTable>
           <StyledTableHead>
             <tr>
@@ -72,6 +80,7 @@ const TransactionsHistoryPage = () => {
               <StyledTableName>Date</StyledTableName>
               <StyledTableName>Time</StyledTableName>
               <StyledTableName>Sum</StyledTableName>
+              <StyledTableName>Actions</StyledTableName>
             </tr>
           </StyledTableHead>
           <tbody>
@@ -80,7 +89,17 @@ const TransactionsHistoryPage = () => {
               <StyledTableBody>December salary</StyledTableBody>
               <StyledTableBody>2022-12-28</StyledTableBody>
               <StyledTableBody>19:45</StyledTableBody>
-              <StyledTableBody>700</StyledTableBody>
+              <StyledTableBody>700 /UAH</StyledTableBody>
+              <StyledTableBody>
+                <ActionButtonWrapper>
+                  <EditButton>
+                    <TrPencil style={{ stroke: '#171719' }} width="16" height="16" />
+                  </EditButton>
+                  <DeleteButton>
+                    <TrTrash width="16" height="16" />
+                  </DeleteButton>
+                </ActionButtonWrapper>
+              </StyledTableBody>
             </tr>
           </tbody>
         </StyledTable>
