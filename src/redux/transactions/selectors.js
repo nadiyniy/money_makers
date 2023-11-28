ef2277acbd25f2a57d6420ce2694ffccbe5490f6
pinkyjohnny <136741239+pinkyjohnny@users.noreply.github.com>
2023-11-28 22:49:35 +0200

    fetch and dateParam

--- src/redux/transactions/selectors.js
+++ src/redux/transactions/selectors.js
@@ -1,3 +1,4 @@
 export const selectTransactions = state => state.transaction.transactions;
 export const selectTransactionsLoading = state => state.transaction.isLoading;
 export const selectTransactionsError = state => state.transaction.error;
+export const selectDate = state => state.transaction.inputDate;
